f0db46be102a06049bd16e216d70e6d663c734e2
nikc <82081084+Nikolas5456587@users.noreply.github.com>
2024-06-24 17:38:30 +0200

    Delete src/main/java/cz/stepit/social/service directory

--- src/main/java/cz/stepit/social/service/AvatarUploadException.java
+++
@@ -1,8 +0,0 @@
-package cz.stepit.social.service;
-
-public class AvatarUploadException extends RuntimeException {
-
-    public AvatarUploadException() {
-        super("Cannot upload avatar");
-    }
-}
--- src/main/java/cz/stepit/social/service/SelfFriendException.java
+++
@@ -1,8 +0,0 @@
-package cz.stepit.social.service;
-
-public class SelfFriendException extends RuntimeException {
-
-    public SelfFriendException() {
-        super("Cannot add self as friend.");
-    }
-}
